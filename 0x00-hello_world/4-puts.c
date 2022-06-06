6969baf1a043f190c167d6ecf8424ce87d75df29
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-06 16:20:50 -0700

    Write a C program that prints exact tex, follow by new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio>
+
+/**
+ * Write a C program that prints exact tex, follow by new line
+ *
+ * Text - "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0)
+}
