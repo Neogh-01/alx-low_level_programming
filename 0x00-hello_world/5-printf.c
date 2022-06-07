f5adb2c328c702f2fc741c8925e78a979407582e
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-06 18:11:05 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ *  * Description: main - print out strings to stdout.
+ *  * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
