1e448fbdb21bbd49dc714de31740b8d589481dd3
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-06 17:17:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * Write a C program that prints exact file
  *
- * Text is "Programming is like building a multilingual puzzle"
+ * Text is Programming is like building a multilingual puzzle
  *
  * Return (0) Always success
  *
  */
+
 int main(void)
 
 {
