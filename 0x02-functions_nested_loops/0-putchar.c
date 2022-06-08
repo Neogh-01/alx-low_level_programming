a68218d783648f669268a6bb068fb36b1b8f4b0e
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-08 15:34:03 -0700

    Updated the file I created tsk 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
-#include<stdio.h>
-
+#include "main.h"
 /**
- * * Description: putchar - Prints out string to stdout
+ * * Description: main - putchar Prints out string to stdout
  * * Return: Always 0 (Success)
  **/
 int main(void)
 {
-_putchar("\_putchar\n");
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
 return (0);
 }
