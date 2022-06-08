83bfba7e732e2aad2c6f285a15d2b06b8f20f8a9
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-07 18:30:47 -0700

    Added the header file called main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * * Description: putchar - Prints out string to stdout
+ * * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+_putchar("\_putchar\n");
+return (0);
+}
