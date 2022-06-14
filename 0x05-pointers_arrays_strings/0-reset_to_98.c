50f6a3bf5c3faab2d3b8d5293b87ce177ec4d53c
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-13 20:19:31 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the reference of a pointer
+ * @n: pointer to be updated
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,1 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 void swap (int *a, int *b);
+void reset_to_98(int *n);
+int _putchar(char c);
+#endif /* MAIN_H */
