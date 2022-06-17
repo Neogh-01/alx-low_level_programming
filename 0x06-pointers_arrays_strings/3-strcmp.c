cbb9eddeb9b69168d626d5035c38bc44c5c332f3
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-16 18:57:32 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - a function that compares strings
+ * @s1: first parameter to be compared
+ * @s2: second parameter to be compared
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+		result = strcmp(s1, s2);
+			return (result);
+}
