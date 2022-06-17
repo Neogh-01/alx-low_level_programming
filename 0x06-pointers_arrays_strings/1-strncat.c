310252fadd3bf785cf72351a3ab48441e6190df9
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-16 18:24:53 -0700

    white space removed and nth para

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * _strncat - is a function concatenates two strings
  * @dest: first parameter to be appended
  * @src: second parameter to be appended
- * @n: 
+ * @n: nth parameter to be appended
  * Return: a pointer to the string dest
  */
 char *_strncat(char *dest, char *src, int n)
