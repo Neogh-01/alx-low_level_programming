fbb81267975cbd0029e595a9a7115066367f4f76
Neo Nkadimeng <neo.nkadimeng@yahoo.com>
2022-06-16 17:57:13 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - is a function that appends a string to a string
+ * @dest - first parameter to be appended by src
+ * @src - seceond parameter to be appended by dest
+ * Return: pointer to string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int len = 0, i;
+	while (dest[len])
+		len++;
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
